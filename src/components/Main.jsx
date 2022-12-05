5fff9541c4fc1ac379866e326dbbac97c6f843d4
BryanGonA <bryan.gonzalez_alv@uao.edu.co>
2022-12-04 22:52:31 -0500

    Ajustes de Inicio
    Se implementó, las pantallas para iniciar sesión en la aplicación

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+import Constants from "expo-constants";
+
+
+const Main = () => {
+    return (
+        <View style={{marginTop: 5, flexGrow: 1}}>
+        <Text>Test</Text>
+        </View>
+    );
+    };
+
+export default Main;
